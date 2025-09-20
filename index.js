0f3d2df6ea8793415d1d0ec71f169ec22e92fbe6
XR-official <xrecruit.official@gmail.com>
2025-09-21 01:41:44 +0530

    FILLING THE HTML TEMPLATES

--- index.js
+++ index.js
@@ -2,7 +2,43 @@ const http = require('http');
 const url = require('url');  
 const fs = require('fs');
 
-//SERVER
+/*
+const tempOverview= fs.readFileSync(`${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template/template-overview.html`, "utf-8",);
+const tempProduct= fs.readFileSync(`${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template-product.html`, "utf-8",);
+const tempCard= fs.readFileSync(`${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template-card.html`, "utf-8",);
+*/
+
+const replaceTemplate= (temp, product)=>{
+    let output= temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output= output.replace(/{%IMAGE%}/g, product.image);
+    output= output.replace(/{%PRICE%}/g, product.price);
+    output= output.replace(/{%FROM%}/g, product.from);
+    output= output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output= output.replace(/{%QUANTITY%}/g, product.quantity);
+    output= output.replace(/{%DESCRIPTION%}/g, product.description);
+    output= output.replace(/{%ID%}/g, product.id);
+
+
+    if(!product.organic) output= output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template-product.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/complete-node-bootcamp/1-node-farm/starter/templates/template-card.html`,
+  "utf-8"
+);
+
 
 const data= fs.readFileSync(`${__dirname}/complete-node-bootcamp/1-node-farm/final/dev-data/data.json`, "utf-8",);
 const dataObj = JSON.parse(data);        
@@ -11,34 +47,43 @@ const dataObj = JSON.parse(data);
 //1.CREATE THE SERVER
 //2.RUN THE SERVER
 
-
+//SERVER
 const server= http.createServer((req, res)=>{
     console.log(req.url);
     const pathName=  req.url;
 
 //ROUTING 
 
+    //OVERVIEW PAGE
     if(pathName === '/' || pathName === '/overview' ){
-        res.end('<h1>THIS IS THE OVERVIEW PAGE !!</h1>');
+        res.writeHead(200, {'Content-type':'text/html', });
+
+
+        const cardsHtml= dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        //console.log(cardsHtml);
+        const output= tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        res.end(output);
         return
     }
 
-
+    //PRODUCT PAGE
     else if(pathName === '/product'){
         res.end('<h1>THIS IS THE HOME PAGE !!!</h1>');
         return
     }
 
-
+    //API PAGE
     else if(pathName=== '/api'){
         res.writeHead(200, {'Content-type' : 'application/json'});
         res.end(data);
         return 
     }
 
+    //NOT FOUND PAGE
     else{
         res.writeHead(404, {
-            'Content-type':'text/html'
+            'Content-type':'text/html', 
+            'my-own-header':'Hello-world'
         });
         res.end('<h1>Page not Found</h1>');
         return
